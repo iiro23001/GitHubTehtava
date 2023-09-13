c4b65486afc4f9316baf06100d10205699a53ad8
iiro23001 <142881999+iiro23001@users.noreply.github.com>
2023-09-13 13:44:36 +0300

    Initial commit

---
+++ src/Moro.java
@@ -0,0 +1,24 @@
+		import java.util.Random;
+
+public class Moro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random random = new Random ();
+		
+	//	int randomNumber = random.nextInt (5);
+	//	System.out.println ("Random number between 0-4: " + randomNumber);
+	
+		
+		int randomNumber = random.nextInt (30)+ 1;
+		
+		System.out.println ("Iironi lottorivi numero 1-30 :" + randomNumber);
+		
+		
+		
+			
+		
+		
+	}
+
+}
