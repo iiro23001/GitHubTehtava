bfb1276f07df4a874ce0cc23d4792cf78b952a2b
iiro23001 <142881999+iiro23001@users.noreply.github.com>
2023-09-13 13:47:56 +0300

    Nro1
    Poistetaan aluksi turha rivi

--- src/Moro.java
+++ src/Moro.java
@@ -3,7 +3,7 @@
 public class Moro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Random random = new Random ();
 		
 	//	int randomNumber = random.nextInt (5);
